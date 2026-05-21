ca1ae3ff26abef09f2b82479a351e308e3aaf1de
sharmila13-d <nookadasharmila@gmail.com>
2026-05-21 21:07:20 +0530

    portno

--- backend/server.js
+++ backend/server.js
@@ -50,7 +50,7 @@ Current Resume: ${formData.currentResume}
     }
 
 });
-const PORT=process.env.PORT || 5000;
+const PORT=process.env.PORT || 8080;
 app.listen(PORT, ()=>{
     console.log(`Server is running on ${PORT}`);
 });
