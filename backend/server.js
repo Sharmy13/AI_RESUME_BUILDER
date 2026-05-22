9dc90f7ad49177f125896a15b4f6ad9f62b597d9
sharmila13-d <nookadasharmila@gmail.com>
2026-05-22 15:45:43 +0530

    backend clean

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,10 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url";
 
 dotenv.config();
 
 const app = express();
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 
 app.use(cors());
